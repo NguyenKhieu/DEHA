afa67bf39b2a746f9cd87f047332ed4befb4f596
NguyenKhieu <khieuvannguyen97@gmail.com>
2019-08-06 21:24:23 +0700

    show producttype

---
+++ app/Http/Controllers/LoaiSanPhamController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SanPham;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
+use Validator;
+use Response;
+use App\ProductType;
+use View;
+use App\Repositories\Post\PostRepositoryInterface;
+
+class LoaiSanPhamController extends Controller
+{
+
+    protected $type;
+
+    public function __construct(ProductType $type)
+    {
+        $this->type = $type;
+    }
+
+    protected $rules = [
+        'loaisanpham' => 'required|min:2|max:32|',
+        'noidung' => 'required|min:2|max:500|',
+        'thuonghieu' => 'required'
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $type = ProductType::all();
+        return view('admincp.loaisanpham.list', ['type' => $type]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), $this->rules);
+        if ($validator->fails()) {
+            return Response::json(array('errors' => $validator->getMessageBag()->toArray()));
+        } else {
+            $type = $this->type->add($request->all());
+            return response()->json($type);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+}
